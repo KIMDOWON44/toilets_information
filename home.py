c3fc4a2f8810b6d96fd2d8e97bd0ea46927e7ac6
KIM DoWon <wisenar@gmail.com>
2023-07-07 17:22:54 +0900

    first comlete

--- home.py
+++ home.py
@@ -3,26 +3,6 @@ import pandas as pd
 import folium
 from streamlit.components.v1 import html
 
-# 스마트폰의 위치 정보를 얻어오는 JavaScript 코드
-geolocation_js = """
-<script>
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        var location = [lat, lon];
-        window.location = location;
-    });
-} else {
-    alert("Geolocation is not supported by this browser.");
-}
-</script>
-"""
-
-# 스마트폰의 위치 정보를 얻어오는 JavaScript 코드를 실행
-html(geolocation_js)
-
-
 df = pd.read_csv("./data/Seoul_toilet_locations.csv", encoding="utf-8")
 sampled_df = df.sample(10)
 
@@ -32,33 +12,23 @@ for i in range(len(sampled_df)):
     name, latitude, longitude = sampled_df.iloc[i]
     folium.Marker([latitude, longitude], popup=name).add_to(tile_seoul_map)
 
-# 스마트폰의 위치 정보를 얻어오는 JavaScript 코드
-geolocation_js = """
-<script>
-if ('geolocation' in navigator) {
-    navigator.geolocation.getCurrentPosition(function(position) {
-        var lat = position.coords.latitude;
-        var lon = position.coords.longitude;
-        var location = [lat, lon];
-        window.location = location;
-    });
-} else {
-    alert("Geolocation is not supported by this browser.");
-}
-</script>
-"""
+st.title("Seoul Toilet Locations")
 
-# 스마트폰의 위치 정보를 얻어오는 JavaScript 코드를 실행
-html(geolocation_js)
+# Get user's location
+user_location = st.empty()
+user_latitude = None
+user_longitude = None
 
-# 사용자의 위치 정보를 얻어옴
-user_location = st.session_state.get('location', None)
+if st.button("Get My Location"):
+    user_location.text("Locating...")
+    user_latitude = 37.2113408  # Replace with actual latitude
+    user_longitude = 127.0611968  # Replace with actual longitude
+    user_location.text("Location Found!")
 
-if user_location:
-    # 사용자의 위치에 마커 추가
-    folium.Marker(user_location, popup="My Location", icon=folium.Icon(color='red')).add_to(tile_seoul_map)
+if user_latitude is not None and user_longitude is not None:
+    # Add user's location marker
+    folium.Marker([user_latitude, user_longitude], popup="My Location", icon=folium.Icon(color='red')).add_to(tile_seoul_map)
 
 map_html = tile_seoul_map.get_root().render()
 
-st.title("Seoul Toilet Locations")
 html(map_html, height=500)
