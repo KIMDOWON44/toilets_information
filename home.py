8bd4312834ed78570e3b316ca2537fbf6c87a53e
KIM DoWon <wisenar@gmail.com>
2023-07-11 16:15:59 +0900

    rebuild

--- home.py
+++ home.py
@@ -1,48 +1,49 @@
 import streamlit as st
 import pandas as pd
-import requests
+from streamlit.components.v1 import html
 
 df = pd.read_csv("./data/Seoul_toilet_locations.csv", encoding="utf-8")
 sampled_df = df.sample(10)
 
-kakao_api_key = "KAKAOMAP_API"  # Replace with your Kakao Maps API key
-kakao_map_url = "https://dapi.kakao.com/v2/maps/staticmap"
-map_params = {
-    "center": "126.98,37.55",
-    "level": "3",
-    "width": "800",
-    "height": "500",
-    "markers": "size:mid|color:red|" + "|".join(f"{row['longitude']},{row['latitude']}" for _, row in sampled_df.iterrows()),
-    "key": kakao_api_key
-}
+# Create a template for the Kakao Maps API
+kakao_template = """
+<div id="map" style="width: 100%; height: 500px;"></div>
+<script type="text/javascript" src="https://dapi.kakao.com/v2/maps/sdk.js?appkey=CACAOMAP_API"></script>
+<script>
+    const mapOptions = {{
+        center: new kakao.maps.LatLng({center_latitude}, {center_longitude}),
+        level: 12
+    }};
+    const map = new kakao.maps.Map(document.getElementById('map'), mapOptions);
 
-response = requests.get(kakao_map_url, params=map_params)
+    // Add markers to the map
+    {marker_script}
+</script>
+"""
+
+# Generate the marker script
+marker_script = ""
+for _, row in sampled_df.iterrows():
+    name, latitude, longitude = row
+    marker_script += f"""
+    const marker = new kakao.maps.Marker({{
+        position: new kakao.maps.LatLng({latitude}, {longitude}),
+        map: map
+    }});
+    const infowindow = new kakao.maps.InfoWindow({{ content: '{name}' }});
+    kakao.maps.event.addListener(marker, 'click', function() {{
+        infowindow.open(map, marker);
+    }});
+    """
+
+# Insert the marker script and center coordinates into the template
+kakao_html = kakao_template.format(
+    center_latitude=37.55,  # Replace with actual center latitude
+    center_longitude=126.98,  # Replace with actual center longitude
+    marker_script=marker_script
+)
 
 st.title("Seoul Toilet Locations")
 
-# Get user's location
-user_location = st.empty()
-user_latitude = None
-user_longitude = None
-
-if st.button("Get My Location"):
-    user_location.text("Locating...")
-    user_latitude = 37.2113408  # Replace with actual latitude
-    user_longitude = 127.0611968  # Replace with actual longitude
-    user_location.text("Location Found!")
-
-if user_latitude is not None and user_longitude is not None:
-    # Append user's location to the marker list
-    map_params["markers"] += f"|{user_longitude},{user_latitude}"
-    response = requests.get(kakao_map_url, params=map_params)
-
-if response.status_code == 200:
-    st.image(response.content, width=800)
-else:
-    st.error("Failed to retrieve the map image.")
-
-content_type = response.headers.get('Content-Type')
-if content_type and content_type.startswith('image/'):
-    st.image(response.content, width=800)
-else:
-    st.error("The response is not a valid image.")
+# Display the Kakao Maps HTML using the `html` component
+html(kakao_html)
